f83bae29784cf06afc4939e5960c896841411d87
TheoDeAna <33116116+TheoDeAna@users.noreply.github.com>
2017-10-27 15:49:56 +0200

    Comparaison
    Comparaison de deux nombres (< ; > ; = ; opposé).

---
+++ Comparaison.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float a, b;
+Comparaison(a,b){
+	if (a<b){
+		return a<b;
+		printf("Le nombre %f", a "est plus petit que %f", b);
+	}else if(a>b){
+		return a>b;
+		printf("Le nombre %f", a "est plus grand que %f", b);
+	}else if(a == b){
+		return a = b;
+		printf("Le nombre %f", a "est égal au nombre %f", b);
+	}else if(a == -b || -a == b){
+		return a == -b
+		printf("Le nombre %f", a "est l'opposé du nombre %f", b);
+	}
+}
+
+int main(){
+	Comparaison(a,b);
+return 0;
+}
