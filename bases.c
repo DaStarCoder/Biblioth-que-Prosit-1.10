e4e8131278ab52b52ea870b39e823456323c3d4c
DaStarCoder <33102136+DaStarCoder@users.noreply.github.com>
2017-10-28 15:33:29 +0200

    Ajout de l'enum etatResultat
    ainsi que réajout du .layout au gitignore, parce que je sais pas pourquoi c'est pas reconnu.

--- bases.c
+++ bases.c
@@ -7,24 +7,29 @@ CE FICHIER CONTIENT LA STRUCTURE DES FRACTIONS UTILISÉES DANS TOUT LE PROGRAMME 
 
 
 // Ceci est la structure contenant les nombres avec lesquels nous travaillons. Les entiers seront représentés par une fraction sur 1.
-typedef struct fraction				
+typedef struct fraction
 {
 	int numerateur;			//Rappel de 6ème: une fraction c'est numérateur / dénominateur.
 	int denominateur;
 } Fraction;
 
+// Cet énumération représente l'état d'un résultat.
+typedef enum etatresultat {
+    succes, // = int 0
+    erreur  // = int 1
+} etatResultat;
 
 // Structure représentant le résultat d'une opération. Elle permet d'indiquer s'il y a eu une erreur, ou le résultat.
 // Veuillez retourner une valeur de ce type à la fin d'un calcul!
-typedef struct resultat				
+typedef struct resultat
 {
-	int estErreur;			// Mettre à 0 s'il y a un résultat, à 1 s'il y une erreur. (Ex. d'utilisation: ÷0)
+	etatResultat estErreur;
 	Fraction resultat;
 } Resultat;
 
 
 // Si qq'un a besoin, ceci permet de créer une Fraction avec deux entiers.
-Fraction Frac(int n, int d)	
+Fraction Frac(int n, int d)
 {
 	Fraction f = { n,d };
 	return f;
@@ -32,7 +37,7 @@ Fraction Frac(int n, int d)
 
 
 // Si qq'un a besoin, ceci permet d'obtenir l'approximation décimale d'une fraction.
-double Decimal(Fraction f)	
+double Decimal(Fraction f)
 {
 	return (double)f.numerateur / f.denominateur;
 }
