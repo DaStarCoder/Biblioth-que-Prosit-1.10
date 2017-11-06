1bfa1fd998857687a6ea8deeea348b2d7323b067
DaStarCoder <33102136+DaStarCoder@users.noreply.github.com>
2017-11-06 11:28:50 +0100

    Corrections

--- Comparaison.h
+++ Comparaison.h
@@ -4,16 +4,4 @@
 
 // 0 signifie que a = b
 // 1 signifie que a est plus petit que b
-int compare(int a, int b){
-	if(a == b){
-		return 0;
-	}else if (a < b){ // 2 signifie que a est plus grand que b
-		return 1; // 3 signifie que a et b sont opposés et que a est plus petit que b
-	}else if(a>b){ // 4 signifie que a et b sont opposés et que a est plus grand que b
-		return 2;
-	}else if (a == -b && a<b){
-		return 3;
-	}else if(a == -b && a>b){
-		return 4;
-	}
-}
+int compare(int a, int b);
