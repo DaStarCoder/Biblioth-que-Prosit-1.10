5e46759f43939e5009303c2912544c70db46b955
ruelune <33153768+ruelune@users.noreply.github.com>
2017-11-06 11:44:30 +0100

    Factorielle

---
+++ factorielle.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bases.h"
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+Resultat factorial (Fraction f)
+{
+	if(f.denominateur==1){
+		int i;
+		long rslt;
+		for(i=1,i<=f.nominateur,i++){
+			rslt*=i;
+		}
+		Fraction x={rslt,1};
+		Resultat r={etatSucces,x};
+		return r;
+	}
+	else{
+		Resultat r={etatErreur,NULL};
+		return r;
+	}
+	
+}
