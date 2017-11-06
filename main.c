9682d8fa8ca3d0316726b15f3e2e2acf6183b8b2
DaStarCoder <33102136+DaStarCoder@users.noreply.github.com>
2017-11-06 11:56:58 +0100

    Remplissage de la fonction main()

--- main.c
+++ main.c
@@ -1,8 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "bases.h"
+#include "arithmetique.h"
+#include "factorielle.h"
+#include "Comparaison.h"
+#include "puissance.h"
+
+Fraction entrerFrac() {
+	Fraction f = { 0, 0 };
+	printf("Entrer numerateur: ");
+	scanf("%d", &(f.numerateur));
+	printf("Entrer denominateur: ");
+	scanf("%d", &(f.denominateur));
+	return f;
+}
+
+void PrintfFrac(Fraction f) {
+	printf("\n%d\n-------\n%d\n", f.numerateur, f.denominateur);
+}
 
 int main()
 {
-    printf("Hello world!\n");
+	printf("Entrez le numéro de l'opération voulue:\n"
+		"0\tAddition\n"
+		"1\tSoustraction\n"
+		"2\tMultiplication\n"
+		"3\tDivision\n"
+		"4\tModulo\n"
+		"5\tPuissance\n"
+		"6\tRacine\n"
+		"7\tComparer\n"
+		"8\tPGCD\n"
+		"9\tPPCM\n"
+		"10\tSimplifier\n"
+		"11\tFactorielle\n");
+	int entree;
+	scanf("%d\n";&entree);
+	
+	switch (entree)
+	{
+	//case 0:
+	//	Fraction f1 = entrerFrac();
+	//	Fraction f2 = entrerFrac();
+	//	PrintfFrac(somme(f1, f2));
+	//case 1:
+	//	Fraction f1 = entrerFrac();
+	//	Fraction f2 = entrerFrac();
+	//	PrintfFrac(difference(f1, f2));
+	//case 2:
+	//	Fraction f1 = entrerFrac();
+	//	Fraction f2 = entrerFrac();
+	//	PrintfFrac(produit(f1, f2));
+	//case 3:
+	//	Fraction f1 = entrerFrac();
+	//	Fraction f2 = entrerFrac();
+	//	PrintfFrac(quotient(f1, f2));
+	//case 4:
+	//	Fraction f1 = entrerFrac();
+	//	Fraction f2 = entrerFrac();
+	//	PrintfFrac(modulo(f1, f2));
+	//case 5:
+	//	Fraction f1 = entrerFrac();
+	//	Fraction f2 = entrerFrac();
+	//	PrintfFrac(puissance(f1, f2));
+	//case 6:
+	//	Fraction f1 = entrerFrac();
+	//	Fraction f2 = entrerFrac();
+	//	PrintfFrac(racine(f1, f2));
+	case 7:
+		Fraction f1 = entrerFrac();
+		Fraction f2 = entrerFrac();
+		printf("%d\n",compare(f1, f2).resultat);
+	//case 8:
+	//	Fraction f1 = entrerFrac();
+	//	Fraction f2 = entrerFrac();
+	//	PrintfFrac(pgcd(f1, f2));
+	//case 9:
+	//	Fraction f1 = entrerFrac();
+	//	Fraction f2 = entrerFrac();
+	//	PrintfFrac(ppcm(f1, f2));
+
+	//case 10:
+	//	Fraction f1 = entrerFrac();
+	//	PrintfFrac(simplifier(f1));
+	case 11:
+		Fraction f1 = entrerFrac();
+		PrintfFrac(factorial(f1));
+	default:
+		printf("Mauvais choix\n");
+		break;
+	}
     return 0;
 }
