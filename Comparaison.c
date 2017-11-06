f3b4a9804d2b9a5f3ebdee9f3e02d23943491e93
TheoDeAna <33116116+TheoDeAna@users.noreply.github.com>
2017-11-06 12:48:12 +0100

    LATEST VERSION
    Grosse modification où les nombres deviennent des fractions

--- Comparaison.c
+++ Comparaison.c
@@ -2,26 +2,50 @@
 #include <stdlib.h>
 #include "Comparaison.h"
 
-//ResultatInt compare(a, b){
-	
-	
-//return 0;
-//}
+// 0 signifie que f1 = f2
+// 1 signifie que f1 est plus petit que f2
+// 2 signifie que f1 est plus grand que f2
+// 3 signifie que f1 et f2 sont opposés et que f1 est plus petit que f2
+// 4 signifie que f1 et f2 sont opposés et que f1 est plus grand que f2
+int PPCM (int a, int b){
+        int r ,i,p;
+        r=a%b;
+        if (r==0){
+            return b;
 
-int compare(int a, int b) {
-	if (a == b) {
+        }else{
+        while (r !=0){
+            i=r;
+            r=b%r;
+        }
+   }
+p = (a*b)/i;
+
+    return p;
+   }
+
+int compare(Fraction a, Fraction b){ 
+Fraction f1 = a;
+Fraction f2 = b;
+int denocommun = PPCM(f1.denominateur, f2.denominateur);
+int multiplicateur1 = denocommun / f1.denominateur;
+int multiplicateur2 = denocommun / f2.denominateur;
+f1.numerateur = f1.numerateur * multiplicateur1;
+f2.numerateur = f2.numerateur * multiplicateur2;
+
+	if (f1.numerateur == f2.numerateur) {
 		return 0;
 	}
-	else if (a < b) { // 2 signifie que a est plus grand que b
-		return 1; // 3 signifie que a et b sont opposés et que a est plus petit que b
+	else if (f1.numerateur < f2.numerateur) { 
+		return 1; 
 	}
-	else if (a>b) { // 4 signifie que a et b sont opposés et que a est plus grand que b
+	else if (f1.numerateur > f2.numerateur) { 
 		return 2;
 	}
-	else if (a == -b && a<b) {
+	else if (f1.numerateur == -f2.numerateur && f1.numerateur < f2.numerateur) {
 		return 3;
 	}
-	else if (a == -b && a>b) {
+	else if (f1.numerateur == -f2.numerateur && f1.numerateur > f2.numerateur) {
 		return 4;
 	}
 }
--- Comparaison.h
+++ Comparaison.h
@@ -2,6 +2,4 @@
 #include <stdlib.h>
 #include "bases.h"
 
-// 0 signifie que a = b
-// 1 signifie que a est plus petit que b
-int compare(int a, int b);
+int compare(Fraction a, Fraction b);
